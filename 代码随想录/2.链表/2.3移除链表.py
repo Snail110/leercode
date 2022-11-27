1264ceec4d37f1287dee99d31633a2299c432598
julianxu <xuzyhubu@163.com>
2022-11-27 18:06:51 +0800

    --story最终整理

--- 代码随想录/2.链表/2.3移除链表.py
+++ 代码随想录/2.链表/2.3移除链表.py
@@ -6,6 +6,19 @@ class ListNode:
 
 """
 题意：删除链表中等于给定值 val 的所有节点。
+
+示例 1：
+输入：head = [1,2,6,3,4,5,6], val = 6
+输出：[1,2,3,4,5]
+
+示例 2：
+输入：head = [], val = 1
+输出：[]
+
+示例 3：
+输入：head = [7,7,7,7], val = 7
+输出：[]
+
 """
 class Solution:
     def replaceListNode(self, head : ListNode, val : int):
